045bbf33d6f76a8bcf64ebc73d93d63b570a0493
Dale O'Brien <lost.blue.whale@gmail.com>
2012-05-01 22:06:24 +1000

    added a new function, returning a dictionary

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -12,10 +12,33 @@ static PyObject * print_hello_world(PyObject *self, PyObject *args){
 
 }
 
+static PyObject * dict_in_r_dict_out(PyObject *self, PyObject *args){
+    
+    // TODO: extract dict from input 
+
+    PyObject *dict = PyDict_New();
+
+    PyObject *x = PyFloat_FromDouble(1.0); 
+    PyObject *z = PyFloat_FromDouble(2.0); 
+
+    PyDict_SetItem(dict, x, z);
+
+    Py_DECREF(x);
+    Py_DECREF(z);
+
+    return Py_BuildValue("O", dict);
+    
+}
+
+
+
 static PyMethodDef HelloWorldMethods[] = {
-    {"printHelloWorld",  print_hello_world, METH_VARARGS, "Prints hello World"},
+    {"printHelloWorld",  print_hello_world, METH_VARARGS, "Prints Hello World"},
+    {"dict_to_dict",  dict_in_r_dict_out, METH_VARARGS, "Swaps values & keys, (will kill dups values)"},
     {NULL, NULL, 0, NULL}
 };
+
+
 
 PyMODINIT_FUNC inithelloWorld(void){
     (void) Py_InitModule("helloWorld", HelloWorldMethods);
--- runme.py
+++ runme.py
@@ -3,3 +3,6 @@ import helloWorld
 
 # should return: set('hello')
 print helloWorld.printHelloWorld()
+
+print
+print helloWorld.dict_to_dict()
